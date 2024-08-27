2b1f2ef27c8f1432c225d1bf5b86458fc8756c08
Dilshod <dilshod.akk2@gmail.com>
2024-08-27 17:48:53 +0500

    started refactoring classes for 3 project

---
+++ SimpleRestaurant2/RefactoredModels/Chicken.cs
@@ -0,0 +1,38 @@
+﻿using SimpleRestaurant2.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleRestaurant2.RefactoredModels
+{
+    public sealed class Chicken : CookedFood
+    {
+        public Chicken(int quantity) : base(quantity) { }
+        public override void Cook()
+        {
+            for (int i = 0; i < _quantity; i++)
+            {
+                CutUp();
+            }
+
+            base.Cook();
+        }
+
+        public override string Serve()
+        {
+            return $"{_quantity} chicken was cooked";
+        }
+
+        public void CutUp()
+        {
+            
+        }
+
+        public override void Request()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ SimpleRestaurant2/RefactoredModels/CookedFood.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleRestaurant2.RefactoredModels
+{
+    public abstract class CookedFood : IMenuItem
+    {
+        protected int _quantity;
+        protected bool _cooked;
+
+        public CookedFood(int quantity)
+        {
+            if (quantity < 0)
+            {
+                throw new ArgumentOutOfRangeException("Quantity must be more than 0");
+            }
+
+            _quantity = quantity;
+        }
+
+        public int GetQuantity() => _quantity;
+
+        public virtual void Cook()
+        {
+            if (_cooked)
+            {
+                throw new Exception("The order was already cooked");
+            }
+
+            _cooked = true;
+        }
+
+        public abstract void Request();
+
+        IMenuItem IMenuItem.Obtain()
+        {
+            throw new NotImplementedException();
+        }
+
+        public abstract string Serve();
+    }
+}
